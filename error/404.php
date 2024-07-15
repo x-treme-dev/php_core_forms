058536982dfb4bb944db513fb51353deb0040688
engineer <newgalaxy401@gmail.com>
2024-07-15 14:25:16 +0300

    add functional

---
+++ error/404.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error 404</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="error__h1">Page not found!</h1>
+    </div>
+</body>
+</html>
--- main.php
+++ main.php
@@ -2,11 +2,32 @@
     <div class="container">
         <ul class="menu">
             <?php
-                $arrMenu = ['Главная' => '#', 'Новости' => 'resource/news.php', 'Напишите нам' => 'resource/writetous.php', 'Логин' => 'resource/login.php'];
+                $arrMenu = ['Главная' => '', 'Новости' => 'news', 'Обратная связь' => 'writetous', 'Логин' => 'login'];
+            
                 foreach($arrMenu as $key => $value){
                     if($key == 'Главная') echo '<li class="menu__li">' . $key . '</li>';
-                    else echo '<li class="menu__li"><a class="menu__a" href="' . $value . '">' . $key . '</a></li>';
+                    else{
+                        echo '<li class="menu__li"><a class="menu__a" href="index.php?page=' . $value . '">' . $key . '</a></li>';
+                    } 
                 }
+
+                switch($_GET['page']){
+                    case 'news': 
+                        header('Location:/resource/news.php');
+                        break;
+                    case 'writetous':
+                        header('Location:/resource/writetous.php');
+                        break;
+                    case 'login':
+                        header('Location:/resource/login.php');
+                        break; 
+                    default:
+                      if( ! $_GET['page'] != '/'){
+                           header('Location:/error/404.php');
+                           break;
+                      }
+                   
+                }    
             ?>
         </ul>
     </div>
